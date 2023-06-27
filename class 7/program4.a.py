38460b8f07b139771949be67da95b9781b05d29b
KIRAN-KUMAR-K3 <18kirankumar.k03@gmail.com>
2023-06-27 11:41:27 +0530

    CLASS 6TH NOTES

---
+++ class 7/program4.a.py
@@ -0,0 +1,16 @@
+#4 A) Write a python program to implement insertion sort and merge sort using lists
+#Insertion Sort
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+    while j >= 0 and key < arr[j]:
+        arr[j + 1] = arr[j]
+        j = j - 1
+# Place key at after the element just smaller than it. 
+    arr[j + 1] = key
+    
+arr = [9, 8, 6, 7, 1]
+print("Unsorted Array:", arr)
+insertion_sort(arr)
+print('Sorted Array: ', arr)
---
+++ class 7/program4.b.py
@@ -0,0 +1,28 @@
+#4 B) Write a program to convert roman numbers in to integer values using
+#dictionaries. 
+roman_numerals = {"I" : 1, 
+                  "V" : 5, 
+                  "X" : 10, 
+                  "L" : 50, 
+                  "C" : 100, 
+                  "D" : 500, 
+                  "M" : 1000
+
+}
+
+int_value = 0
+
+user_input = input("Enter Roman Number: ").upper()
+
+for i in range(len(user_input)):
+    if user_input[i] in roman_numerals:
+     if i + 1 < len(user_input) and roman_numerals[user_input[i]] <roman_numerals[user_input[i + 1]]:
+        int_value -= roman_numerals[user_input[i]]
+    else:
+        int_value += roman_numerals[user_input[i]]
+else:
+
+    print("Invalid input.")
+    quit()
+    
+print("The integer value is: ", int_value)
